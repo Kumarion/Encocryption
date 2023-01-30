87f221afebbf63455f08abf9e8c26b0fe6c641cd
Kumarion <121711454+Kumarion@users.noreply.github.com>
2023-01-29 21:04:13 -0500

    Added Base91 support

--- StringEncoder.lua
+++ StringEncoder.lua
@@ -5,6 +5,11 @@ local base64chars = [[
 	ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/
 ]];
 
+-- Define our base91 characters (recommended for use with datastores)
+local base91chars = [[
+	ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,-./:;<=>?@[]^_`{|}~
+]];
+
 function stringToBase64(str)
 	local serialized = str;
 
@@ -45,7 +50,109 @@ function base64ToString(base64)
 	return serialized;
 end
 
+local function stringToBase91(str)
+	if (str == nil) then
+		error("Base91: Missing str to encode.");
+	end;
+
+	local raw;
+
+	if (type(str) == "number") then
+		raw = tostring(str);
+	else
+		raw = str;
+	end;
+
+	raw = { raw:byte(1, #raw) };
+	local len = #raw;
+
+	local ret = "";
+	local n = 0;
+	local b = 0;
+
+	for i = 1, len do
+		b = bit32.bor(b, bit32.lshift(raw[i], n));
+		n = n + 8;
+
+		if (n > 13) then
+			local v = bit32.band(b, 8191);
+
+			if (v > 88) then
+				b = bit32.rshift(b, 13);
+				n = n - 13;
+			else
+				v = bit32.band(b, 16383);
+				b = bit32.rshift(b, 14);
+				n = n - 14;
+			end
+
+			ret = ret .. string.char(base91chars:byte(v % 91 + 1)) .. string.char(base91chars:byte(math.floor(v / 91) + 1));
+		end;
+	end;
+
+	if (n > 0) then
+		ret = ret .. string.char(base91chars:byte(b % 91 + 1));
+
+		if (n > 7 or b > 90) then
+			ret = ret .. string.char(base91chars:byte(math.floor(b / 91) + 1));
+		end;
+	end;
+
+	return ret;
+end
+
+local function base91ToString(base91)
+	local raw = tostring(base91 or "");
+	local len = #raw;
+
+	local ret = {};
+	local b = 0;
+	local n = 0;
+	local v = -1;
+
+	for i = 1, len do
+		local p = string.find(base91chars, raw:sub(i, i), 1, true);
+
+		if (p == nil) then
+			continue;
+		end;
+
+		if v < 0 then
+			v = p - 1;
+		else
+			v = v + (p - 1) * 91;
+			b = b + bit32.lshift(v, n);
+			n = n + (bit32.band(v, 8191) > 88 and 13 or 14);
+
+			while (n > 7) do
+				table.insert(ret, bit32.band(b, 0xff));
+				b = bit32.rshift(b, 8);
+				n = n - 8;
+			end;
+
+			v = -1;
+		end;
+
+		continue;
+	end;
+
+	if (v > -1) then
+		table.insert(ret, bit32.band(b + bit32.lshift(v, n), 0xff));
+	end;
+
+	local decoded = "";
+
+	for i, byte in ipairs(ret) do
+		decoded = decoded .. string.char(byte);
+	end;
+
+	return decoded;
+end
+
 return {
-	Encode = stringToBase64,
-	Decode = base64ToString,
+	Encode64 = stringToBase64,
+	Decode64 = base64ToString,
+
+	Encode91 = stringToBase91,
+	Decode91 = base91ToString,
 }
--- StringEncoderTest.lua
+++ StringEncoderTest.lua
@@ -2,24 +2,44 @@
 local StringEncoder = require(game:GetService("ReplicatedStorage").StringEncoder);
 
 -- Define the test
-local function StringEncoderTest()
-    -- Define the test string
-    local testString = "Hello World!";
+local function StringEncoder64Test()
+	-- Define the test string
+	local testString = "Hello World!";
 
-    -- Encode the string
-    local encodedString = StringEncoder.Encode(testString);
+	-- Encode the string
+	local encodedString = StringEncoder.Encode64(testString);
 
-    -- Decode the string
-    local decodedString = StringEncoder.Decode(encodedString);
+	-- Decode the string
+	local decodedString = StringEncoder.Decode64(encodedString);
 
-    -- Print the results
-    print("Test String: " .. testString);
-    print("Encoded String: " .. encodedString);
-    print("Decoded String: " .. decodedString);
+	-- Print the results
+	print("Test String: " .. testString);
+	print("Encoded String: " .. encodedString);
+	print("Decoded String: " .. decodedString);
 
-    -- Assert the results
-    assert(testString == decodedString, "The decoded string does not match the original string.");
+	-- Assert the results
+	assert(testString == decodedString, "The decoded string does not match the original string.");
+end
+
+local function StringEncoder91Test()
+	-- Define the test string
+	local testString = "Hello World! How are you!!";
+
+	-- Encode the string
+	local encodedString = StringEncoder.Encode91(testString);
+
+	-- Decode the string
+	local decodedString = StringEncoder.Decode91(encodedString);
+
+	-- Print the results
+	print("Test String: " .. testString);
+	print("Encoded String: " .. encodedString);
+	print("Decoded String: " .. decodedString);
+
+	-- Assert the results
+	assert(testString == decodedString, "The decoded string does not match the original string.");
 end
 
 -- Run the test
-StringEncoderTest();
+--StringEncoder64Test();
+StringEncoder91Test();
