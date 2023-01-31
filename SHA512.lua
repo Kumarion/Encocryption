091b74eaf5d0d1a8e19dd1dde19a246886748fc2
Kumarion <121711454+Kumarion@users.noreply.github.com>
2023-01-31 05:16:27 -0500

    partially uploaded a sha512 encryption for lua, not fully done though

--- SHA512.lua
+++ SHA512.lua
@@ -1,7 +1,7 @@
 -- sha512 function
 
 -- initial values
-local t = {
+local _h = {
     0x6a09e667f3bcc908,
     0xbb67ae8584caa73b,
     0x3c6ef372fe94f82b,
@@ -13,7 +13,7 @@ local t = {
 }
 
 -- initial constants
-local CONSTANTS = {
+local _k = {
     0x428a2f98d728ae22, 0x7137449123ef65cd, 0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc,
     0x3956c25bf348b538, 0x59f111f1b605d019, 0x923f82a4af194f9b, 0xab1c5ed5da6d8118,
     0xd807aa98a3030242, 0x12835b0145706fbe, 0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2,
@@ -36,70 +36,92 @@ local CONSTANTS = {
     0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817
 };
 
+local MAX_64BIT = 0xffffffffffffffff;
+
+local function _rotr(x, y)
+    return (bit32.band((bit32.bor((bit32.rshift(x, y)), (bit32.lshift(x, (64 - y))))), MAX_64BIT));
+end
+
 function SHA512(data)
-    local main = t
-    data = data .. "\128" .. string.rep("\0", 127 - (#data + 8) % 128) .. string.pack(">I8", #data * 8)
-  
-    for i = 1, #data, 128 do
-      local chunk = data:sub(i, i + 127)
-      local w = {}
-  
-      for j = 1, 16 do
-        w[j] = string.unpack(">I8", chunk, (j - 1) * 8 + 1)
-      end
-  
-      for j = 17, 80 do
-        w[j] = bit32.bxor(bit32.rrotate(w[j - 15], 1), bit32.rrotate(w[j - 15], 8), bit32.rshift(w[j - 15], 7)) + w[j - 16] + bit32.bxor(bit32.rrotate(w[j - 2], 19), bit32.rrotate(w[j - 2], 61), bit32.rshift(w[j - 2], 6)) + w[j - 7]
-      end
-  
-      local a, b, c, d, e, f, g, h = t[1], t[2], t[3], t[4], t[5], t[6], t[7], t[8]
-  
-      for j = 1, 80 do
-        local t1 = h + bit32.bxor(bit32.rrotate(e, 14), bit32.rrotate(e, 18), bit32.rrotate(e, 41)) + bit32.bxor(bit32.band(e, f), bit32.band(bit32.bnot(e), g)) + w[j]
-        local t2 = bit32.bxor(bit32.rrotate(a, 28), bit32.rrotate(a, 34), bit32.rrotate(a, 39)) + bit32.bxor(bit32.band(a, b), bit32.band(a, c), bit32.band(b, c))
-        h = g
-        g = f
-        f = e
-        e = bit32.band(d + t1, 0xffffffffffffffff)
-        d = c
-        c = b
-        b = a
-        a = bit32.band(t1 + t2, 0xffffffffffffffff)
-      end
-  
-        main[1] = bit32.band(t[1] + a, 0xffffffffffffffff)
-        main[2] = bit32.band(t[2] + b, 0xffffffffffffffff)
-        main[3] = bit32.band(t[3] + c, 0xffffffffffffffff)
-		main[4] = bit32.band(main[4] + d, 0xffffffff);
-		main[5] = bit32.band(main[5] + e, 0xffffffff);
-		main[6] = bit32.band(main[6] + f, 0xffffffff);
-		main[7] = bit32.band(main[7] + g, 0xffffffff);
-		main[8] = bit32.band(main[8] + h, 0xffffffff);
+    local main = {};
+
+    -- populate the main array
+	for i = 1, #_h do
+		main[i] = _h[i];
 	end;
 
-	-- return the digest
-	return {
-		hex = function()
-            local digest = "";
+    local function _sha512_process(chunk)
+        local w = {};
 
-            for i = 1, #main do
-                digest = digest .. string.format("%08x", main[i]);
+        -- fill the w array
+        for j = 1, 16 do
+            -- get the 32 bit word
+            local word = chunk:sub((j - 1) * 8 + 1, j * 8);
+
+            -- pad the word if it's too short
+            if #word < 8 then
+                word = word .. string.rep("\0", 8 - #word);
             end;
 
-            return digest;
-		end,
+            -- convert the word to a number in big endian
+            w[j] = string.unpack(">I8", word);
+        end;
 
-		bytes = function()
-			local digest = "";
+        -- print(w)
+        print(w);
 
-			for i = 1, #main do
-				digest = digest .. string.pack(">I4", main[i]);
-			end;
+        -- extend the w array
+        for i = 17, 80 do
+            local s0 = bit32.bxor(bit32.bxor(_rotr(w[i - 15], 1), _rotr(w[i - 15], 8)), bit32.rshift(w[i - 15], 7));
+            local s1 = bit32.bxor(bit32.bxor(_rotr(w[i - 2], 19), _rotr(w[i - 2], 61)), bit32.rshift(w[i - 2], 6));      
+            w[i] = bit32.band(w[i - 16] + s0 + w[i - 7] + s1, MAX_64BIT);
+        end;
 
-			return digest;
-		end;
-	};
-end;
+        local a, b, c, d, e, f, g, h = main[1], main[2], main[3], main[4], main[5], main[6], main[7], main[8];
 
--- test
-print(SHA512("unicorn").hex());
+        -- main loop
+        for i = 1, 80 do
+            local s0 = bit32.bxor(bit32.bxor(_rotr(a, 28), _rotr(a, 34)), _rotr(a, 39));
+            local maj = bit32.bxor(bit32.bxor(bit32.band(a, b), bit32.band(a, c)), bit32.band(b, c));
+            local t2 = bit32.band(s0 + maj, MAX_64BIT);
+            local s1 = bit32.bxor(bit32.bxor(_rotr(e, 14), _rotr(e, 18)), _rotr(e, 41));
+            local ch = bit32.bxor(bit32.band(e, f), bit32.band(bit32.bnot(e), g));
+            local t1 = bit32.band(h + s1 + ch + _k[i] + w[i], MAX_64BIT);
+
+            h = g;
+            g = f;
+            f = e;
+            e = bit32.band(d + t1, MAX_64BIT);
+            d = c;
+            c = b;
+            b = a;
+            a = bit32.band(t1 + t2, MAX_64BIT);
+        end;
+
+        print("END OF LOOP");
+        print(a,b,c,d,e,f,g,h);
+
+        main[1] = bit32.band(main[1] + a, MAX_64BIT);
+        main[2] = bit32.band(main[2] + b, MAX_64BIT);
+        main[3] = bit32.band(main[3] + c, MAX_64BIT);
+        main[4] = bit32.band(main[4] + d, MAX_64BIT);
+        main[5] = bit32.band(main[5] + e, MAX_64BIT);
+        main[6] = bit32.band(main[6] + f, MAX_64BIT);
+        main[7] = bit32.band(main[7] + g, MAX_64BIT);
+        main[8] = bit32.band(main[8] + h, MAX_64BIT);
+    end;
+
+    for i = 1, #data, 128 do
+        _sha512_process(data:sub(i, i + 127));
+    end;
+
+    local digest = "";
+
+    for i = 1, #main do
+        digest = digest .. string.format("%02x", main[i]);
+    end;
+
+    return digest;
+end
+
+print(SHA512("unicorn"));
